ba9fbd2cab0ba5785354ba0ff997db4c626f428e
Denis Silantev <d.silantjev2012@gmail.com>
2020-10-04 22:19:42 +0300

    add h w

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 function scripts() {
   // вставляем стили для скрытия блоков
   $("body *:first").before(
-    "<style>" + "noindex { display: none !important; }" + "</style>",
+    "<style>" +
+      "noindex { display: none !important; width: 0px !important; height: 0px !important;}" +
+      "</style>",
   );
+  $().addClass;
 }
