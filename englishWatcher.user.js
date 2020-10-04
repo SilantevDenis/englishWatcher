851813f75db8b4484de956d44c53660804e759ea
Denis Silantev <d.silantjev2012@gmail.com>
2020-10-04 23:50:21 +0300

    remove scripts.js

--- englishWatcher.user.js
+++ englishWatcher.user.js
@@ -7,10 +7,44 @@
 // @match        https://zloekino.su/*
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.5.1.slim.js
-// @require      https://raw.githubusercontent.com/SilantevDenis/englishWatcher/master/scripts.js
 // ==/UserScript==
 
 (function () {
   "use strict";
-  $(document).ready(scripts);
+  $(document).ready(() => {
+    /// постоянно скрываем рекламу
+    let timer = setInterval(() => {
+      $("noindex").parent().hide();
+    }, 100);
+    // прекращаем скрывать рекламу
+    setTimeout(() => {
+      clearTimeout(timer);
+    }, 5000);
+
+    // время перемотки назад
+    let time = 5 * 60 * 1000;
+    // храним положение переключателя субтитров
+    let subtitle = false;
+    // каждые 5 минут фильма
+    setInterval(() => {
+      // если русские субтитры включены
+      if (subtitle) {
+        // отключаем субтитры
+        $("label:contains('Нет')").click();
+        //переключаем переменную
+        subtitle = false;
+      } else {
+        // если субтитры выключены
+        // получаем видео
+        let video = document.getElementsByTagName("video")[0];
+        // перематываем на 5 минут назад
+        video.currentTime -= time / 1000;
+
+        // ставим русские субтитры
+        $("label:contains('Russian')").click();
+        //переключаем переменную
+        subtitle = true;
+      }
+    }, time);
+  });
 })();
