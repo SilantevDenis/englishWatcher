9a4f2d8abe678a23e531bd3daa17ce9a403e9234
Denis Silantev <d.silantjev2012@gmail.com>
2020-10-04 22:42:37 +0300

    correct

--- scripts.js
+++ scripts.js
@@ -1,9 +1,6 @@
 function scripts() {
-  // вставляем стили для скрытия блоков
-  $("body *:first").before(
-    "<style>" +
-      "noindex { display: none !important; width: 0px !important; height: 0px !important;}" +
-      "</style>",
-  );
-  $().addClass;
+  /// скрываем рекламу, когда она появится
+  setTimeout(() => {
+    $("noindex").parent().hide();
+  }, 5000);
 }
