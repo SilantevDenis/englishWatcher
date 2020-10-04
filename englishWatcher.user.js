40f7e0be7eee3f65789758ea113bb97db1f5f6af
Denis Silantev <d.silantjev2012@gmail.com>
2020-10-04 23:17:36 +0300

    first work version

--- englishWatcher.user.js
+++ englishWatcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         englishWatcher
 // @namespace    https://github.com/SilantevDenis/englishWatcher
-// @version      1.0.1
+// @version      1.0.0
 // @description  helps to watch English movies and learn the language
 // @author       silantevdenis
 // @match        https://zloekino.su/*
--- scripts.js
+++ scripts.js
@@ -1,6 +1,38 @@
 function scripts() {
-  /// скрываем рекламу, когда она появится
-  setTimeout(() => {
+  /// постоянно скрываем рекламу
+  let timer = setInterval(() => {
+    console.log(1);
     $("noindex").parent().hide();
+  }, 100);
+  // прекращаем скрывать рекламу
+  setTimeout(() => {
+    console.log(2);
+    clearTimeout(timer);
   }, 5000);
+
+  // время перемотки назад
+  let time = 5 * 60 * 1000;
+  // храним положение переключателя субтитров
+  let subtitle = false;
+  // каждые 5 минут фильма
+  setInterval(() => {
+    // если русские субтитры включены
+    if (subtitle) {
+      // отключаем субтитры
+      $("label:contains('Нет')").click();
+      //переключаем переменную
+      subtitle = false;
+    } else {
+      // если субтитры выключены
+      // получаем видео
+      let video = document.getElementsByTagName("video")[0];
+      // перематываем на 5 минут назад
+      video.currentTime -= time / 1000;
+
+      // ставим русские субтитры
+      $("label:contains('Russian')").click();
+      //переключаем переменную
+      subtitle = true;
+    }
+  }, time);
 }
